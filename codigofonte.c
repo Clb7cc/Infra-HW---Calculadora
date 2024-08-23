f5173234074c367d9c04b8df46591ef410173b38
Clb7cc <clb@cesar.school>
2024-08-23 12:41:31 -0300

    funcao dec para bin

--- codigofonte.c
+++ codigofonte.c
@@ -1,1 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+void dec_Bin(int numx) {
+    int bin[32];
+    int cont = 0;
+
+    
+    while (numx > 0) {
+        bin[cont] = numx % 2;
+        numx = numx / 2;
+        cont++;
+    }
+
+    printf("Binario: ");
+    for (int i = cont - 1; i >= 0; i--) {
+        printf("%d", bin[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int numf;
+
+    printf("Digite um numero decimal: ");
+    scanf("%d", &numf);
+
+    printf("Convertendo o numero %d:\n", numf);
+
+    dec_Bin(numf);
+
+    return 0;
+}
